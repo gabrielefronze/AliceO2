70561e0b7d73f39c353f90f0588142429a57ab7d
Gabriele Fronzé <sucre.91@hotmail.it>
2017-04-28 17:20:17 +0200

    Deserializer modified to mimic iterator

--- Detectors/MUON/base/include/MUONBase/Deserializer.cxx
+++ Detectors/MUON/base/include/MUONBase/Deserializer.cxx
@@ -8,7 +8,7 @@ using namespace AliceO2::MUON;
 
 Deserializer::Deserializer(FairMQMessagePtr& msg):
 fDigitCounter(0),
-fOffset(uint32_t(1)){
+fOffset(1){
     fDataPtr = reinterpret_cast<uint8_t*>(msg->GetData());
     fDigitsDataPtr = reinterpret_cast<uint32_t*>(fDataPtr + 100);
     fNDigits = fDigitsDataPtr[0];
@@ -21,32 +21,29 @@ Deserializer::~Deserializer(){
 
 bool Deserializer::Rewind(){
     fDigitCounter = 0;
-    fOffset = uint32_t(1);
+    fOffset = 1;
 }
 
-bool Deserializer::NextDigit(uint32_t *output) {
+dataStruct* Deserializer::NextDigit() {
 
-    if ( fDigitCounter>fNDigits ) return false;
+    if ( fDigitCounter>fNDigits ) return 0x0;
 
     fDigitCounter++;
     bool control = true;
-    uint32_t outputArray[kNumberOfValues];
     fUniqueID = fDigitsDataPtr[fOffset++];
 
     for (short iData = 0; iData < kNumberOfValues ; ++iData) {
-        control &= ApplyMask(iData,outputArray[iData]);
+        control &= ApplyMask(iData);
         fOffset++;
     }
 
-    output = outputArray;
-
-    return control;
+    return (control) ? &fOutputDataStruct : 0x0;
 }
 
-bool Deserializer::ApplyMask(short maskIndex, uint32_t &returnValue){
+bool Deserializer::ApplyMask(short maskIndex){
     if (maskIndex>(kNumberOfValues-1)) return false;
 
-    returnValue = (fUniqueID & kMasks[maskIndex]) >> kShifts[maskIndex];
+    *(fDataStructItems[maskIndex]) = (fUniqueID & kMasks[maskIndex]) >> kShifts[maskIndex];
 
     return true;
 }
