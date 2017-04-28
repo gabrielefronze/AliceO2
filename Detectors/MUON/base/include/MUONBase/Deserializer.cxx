bbec3293b2a0299e3b2b1e7009c4295cf27ae236
Gabriele Fronzé <sucre.91@hotmail.it>
2017-04-28 17:36:07 +0200

    Added comments to Deserializer class

--- Detectors/MUON/base/include/MUONBase/Deserializer.cxx
+++ Detectors/MUON/base/include/MUONBase/Deserializer.cxx
@@ -6,43 +6,62 @@
 
 using namespace AliceO2::MUON;
 
+//_________________________________________________________________________________________________
 Deserializer::Deserializer(FairMQMessagePtr& msg):
 fDigitCounter(0),
 fOffset(1){
+    // Internal pointers to data are kept locally
     fDataPtr = reinterpret_cast<uint8_t*>(msg->GetData());
     fDigitsDataPtr = reinterpret_cast<uint32_t*>(fDataPtr + 100);
     fNDigits = fDigitsDataPtr[0];
 }
 
+//_________________________________________________________________________________________________
 Deserializer::~Deserializer(){
     fDataPtr = 0x0;
     fDigitsDataPtr = 0x0;
 }
 
+//_________________________________________________________________________________________________
 bool Deserializer::Rewind(){
+    // It is enough to reset the digits counter and the offset
     fDigitCounter = 0;
     fOffset = 1;
 }
 
-dataStruct* Deserializer::NextDigit() {
+//_________________________________________________________________________________________________
+deserializerDataStruct* Deserializer::NextDigit() {
 
+    // Avoid exceding the total number of digits
     if ( fDigitCounter>fNDigits ) return 0x0;
 
+    // Keep track of how much digits have been read
     fDigitCounter++;
-    bool control = true;
-    fUniqueID = fDigitsDataPtr[fOffset++];
 
+    // Control value to detect parsing problems
+    bool control = true;
+
+    // Loading data in the data member used by ApplyMask
+    fUniqueID = fDigitsDataPtr[fOffset];
+
+    // Apply all the masks defined in the header
     for (short iData = 0; iData < kNumberOfValues ; ++iData) {
         control &= ApplyMask(iData);
-        fOffset++;
     }
 
+    // Go to the following digit leaping unwanted data
+    fOffset+=2;
+
+    // If everything is ok return the pointer to the internal dataStruct
     return (control) ? &fOutputDataStruct : 0x0;
 }
 
+//_________________________________________________________________________________________________
 bool Deserializer::ApplyMask(short maskIndex){
+    // Check if the maskIndex is allowed
     if (maskIndex>(kNumberOfValues-1)) return false;
 
+    // Apply the mask
     *(fDataStructItems[maskIndex]) = (fUniqueID & kMasks[maskIndex]) >> kShifts[maskIndex];
 
     return true;
