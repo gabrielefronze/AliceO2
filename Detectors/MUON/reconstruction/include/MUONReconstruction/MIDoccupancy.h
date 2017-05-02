fdefecd5102f4adb7b2a03287cdd2dc47c8ec337
Gabriele Fronzé <sucre.91@hotmail.it>
2017-05-02 11:50:42 +0200

    Creation of MIDoccupancy class

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDoccupancy.h
@@ -0,0 +1,23 @@
+//
+// Created by Gabriele Gaetano Fronzé on 02/05/2017.
+//
+
+#ifndef MIDOCCUPANCY_H
+#define MIDOCCUPANCY_H
+
+#include "FairMQDevice.h"
+
+namespace AliceO2{
+
+    namespace MUON {
+
+        class MIDoccupancy : public FairMQDevice
+        {
+
+        };
+
+    }
+
+}
+
+#endif //MIDOCCUPANCY_H
---
+++ Detectors/MUON/reconstruction/src/MIDoccupancy.cxx
@@ -0,0 +1,5 @@
+//
+// Created by Gabriele Gaetano Fronzé on 02/05/2017.
+//
+
+#include "MUONReconstruction/MIDoccupancy.h"
