76414ef532edf418f5e9c2c551aa9db7f791ab33
Gabriele Fronzé <sucre.91@hotmail.it>
2017-05-02 15:33:43 +0200

    First structure of MIDoccupancy class
    For testing purposes: occupancy map is not yet computed

--- Detectors/MUON/reconstruction/include/MUONReconstruction/MIDoccupancy.h
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDoccupancy.h
@@ -5,14 +5,30 @@
 #ifndef MIDOCCUPANCY_H
 #define MIDOCCUPANCY_H
 
+#include "Rtypes.h"
 #include "FairMQDevice.h"
+#include "MUONBase/Deserializer.h"
+#include "MUONBase/Mapping.h"
+#include "TString.h"
 
-namespace AliceO2{
+namespace AliceO2 {
 
     namespace MUON {
 
         class MIDoccupancy : public FairMQDevice
         {
+        public:
+            MIDoccupancy(TString mapFilename);
+            virtual ~MIDoccupancy();
+
+        protected:
+            bool HandleData(FairMQMessagePtr&, int);
+            virtual void InitTask();
+
+        private:
+            Mapping::mpDE* fInternalMapping;
+            Deserializer fMessageDeserializer;
+            TString fMapFilename;
 
         };
 
--- Detectors/MUON/reconstruction/src/MIDoccupancy.cxx
+++ Detectors/MUON/reconstruction/src/MIDoccupancy.cxx
@@ -3,3 +3,59 @@
 //
 
 #include "MUONReconstruction/MIDoccupancy.h"
+#include "FairMQLogger.h"
+#include "options/FairMQProgOptions.h"
+#include "boost"
+
+using namespace AliceO2::MUON;
+
+//_________________________________________________________________________________________________
+MIDoccupancy::MIDoccupancy(TString mapFilename):
+FairMQDevice(),
+fMessageDeserializer(),
+fInternalMapping(0x0),
+fMapFilename(mapFilename){}
+
+//_________________________________________________________________________________________________
+MIDoccupancy::~MIDoccupancy() {
+    if ( fInternalMapping ) delete fInternalMapping;
+}
+
+//_________________________________________________________________________________________________
+void MIDoccupancy::InitTask() {
+
+    if ( !boost::filesystem::exists(fMapFilename.Data()) ){
+        LOG(ERROR) << "Could not read binary mapping file: " << fMapFilename.Data();
+        return;
+    } else {
+        LOG(TRACE) << "File " << fMapFilename.Data()<<" found.";
+    }
+
+    int numberOfDetectionElements = 0;
+    fInternalMapping = Mapping::ReadMapping(fMapFilename.Data(),numberOfDetectionElements);
+
+    if ( !fInternalMapping ){
+        LOG(ERROR) << "Error reading the mapping even if " << fMapFilename.Data()<<" exists.";
+    } else {
+        LOG(TRACE) << "Mapping correctly loaded with "<<numberOfDetectionElements<<" detector elements.";
+    }
+
+}
+
+//_________________________________________________________________________________________________
+bool MIDoccupancy::HandleData(FairMQMessagePtr &msg, int /*index*/)
+{
+    fMessageDeserializer = Deserializer(msg);
+    int counter = 0;
+
+    Deserializer::deserializerDataStruct* deserializedData;
+
+    while(deserializedData = fMessageDeserializer.NextDigit()){
+        counter++;
+        LOG(TRACE) << "Read "<<counter<<" data:";
+        LOG(TRACE) << "\t"<<deserializedData->fDetElemID;
+        LOG(TRACE) << "\t"<<deserializedData->fBoardID;
+        LOG(TRACE) << "\t"<<deserializedData->fChannel;
+        LOG(TRACE) << "\t"<<deserializedData->fCathode;
+    }
+}
