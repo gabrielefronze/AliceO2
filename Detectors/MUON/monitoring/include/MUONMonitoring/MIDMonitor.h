2aa1b30d92bde28848cefd15203e19d145c9da68
Gabriele Fronzé <sucre.91@hotmail.it>
2017-06-21 18:07:12 +0200

    First commit

---
+++ Detectors/MUON/monitoring/include/MUONMonitoring/MIDMonitor.h
@@ -0,0 +1,22 @@
+//
+// Created by Gabriele Gaetano Fronzé on 23/05/2017.
+//
+
+#ifndef MIDMONITOR_H
+#define MIDMONITOR_H
+
+#include "Rtypes.h"
+#include "TBox.h"
+#include "TCanvas.h"
+
+namespace AliceO2 {
+    namespace MUON (
+        class MIDMonitor {
+
+        };
+    )
+}
+
+
+
+#endif //MIDMONITOR_H
