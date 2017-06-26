241d9e45b351b7b19044f72de8bb2b57fe720541
Gabriele Fronzé <sucre.91@hotmail.it>
2017-06-26 17:40:24 +0200

    Added MIDMaskGenerator device
    This device will receive rates from MIDRatesComputer and detect dead and noisy strips. The Mask will then be forwarded to the MIDFilter device.

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDMaskGenerator.h
@@ -0,0 +1,40 @@
+//
+// Created by Gabriele Gaetano Fronzé on 23/06/2017.
+//
+
+#ifndef O2_DEV_ALO_MIDMASKGENERATOR_H
+#define O2_DEV_ALO_MIDMASKGENERATOR_H
+
+#include "FairMQDevice.h"
+#include "MUONBase/OccupancyMapping.h"
+
+namespace AliceO2 {
+
+    namespace MUON {
+
+        class MIDMaskGenerator : public FairMQDevice, public OccupancyMapping{
+
+        public:
+            MIDMaskGenerator();
+            virtual ~MIDMaskGenerator();
+
+        protected:
+            bool HandleData(FairMQMessagePtr&, int);
+            virtual void InitTask();
+
+        private:
+            struct stripMask{
+                UShort_t nDead; // number of elements for deadStripsIDs
+                UShort_t nNoisy; // number of elements for noisyStripsIDs
+                std::unordered_set<uint32_t> deadStripsIDs; // container of UniqueIDs of dead strips
+                std::unordered_set<uint32_t> noisyStripsIDs; // container of UniqueIDs of noisy strips
+            };
+
+            stripMask fStructMask;
+            stripMask fStructMaskSim;
+        };
+    }
+}
+
+
+#endif //O2_DEV_ALO_MIDMASKGENERATOR_H
---
+++ Detectors/MUON/reconstruction/src/MIDMaskGenerator.cxx
@@ -0,0 +1,5 @@
+//
+// Created by Gabriele Gaetano Fronzé on 23/06/2017.
+//
+
+#include "MUONReconstruction/MIDMaskGenerator.h"
