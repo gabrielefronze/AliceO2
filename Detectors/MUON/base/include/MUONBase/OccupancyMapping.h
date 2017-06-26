6362ad6348f0a309b9a57581f237baf99e55793e
Gabriele Fronzé <sucre.91@hotmail.it>
2017-06-26 15:05:29 +0200

    Added OccupancyMapping class
    This class is used to have the same mapping in different devices

---
+++ Detectors/MUON/base/include/MUONBase/OccupancyMapping.h
@@ -0,0 +1,48 @@
+//
+// Created by Gabriele Gaetano Fronzé on 26/06/2017.
+//
+
+#ifndef O2_DEV_ALO_OCCUPANCYMAPPING_H
+#define O2_DEV_ALO_OCCUPANCYMAPPING_H
+
+#include <array>
+#include <vector>
+#include <unordered_map>
+#include <unordered_set>
+
+namespace AliceO2 {
+
+    namespace MUON {
+
+        class OccupancyMapping {
+        protected:
+            enum digitType {
+                kPhysics,
+                kFET,
+                kTriggered,
+                kSize
+            };
+
+            struct stripMapping {
+                uint64_t startTS[kSize]; // timestamp of first added run
+                uint64_t stopTS[kSize]; // timestamp of last added run
+                uint64_t digitsCounter[kSize]; // counter of time the strip has been fired
+                UShort_t columnID;
+                Float_t area; // 1D area
+                Float_t coord[2][2];
+                Float_t rate[kSize]; // rate in Hz/cm2 or 1/cm2
+                Bool_t isDead;
+                Bool_t isNoisy;
+            };
+
+            std::unordered_map<uint32_t, stripMapping *> fInternalMapping;
+            std::array<stripMapping, 20992> fStripVector;
+            std::vector<stripMapping *> fStructsBuffer;
+
+            bool ReadMapping(const char*);
+        };
+    }
+}
+
+
+#endif //O2_DEV_ALO_OCCUPANCYMAPPING_H
