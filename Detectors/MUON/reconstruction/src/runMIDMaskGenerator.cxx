01de62656dc3ef5a7b4f1a0e20353ac4d4b2e2a2
Gabriele Fronzé <sucre.91@hotmail.it>
2017-07-07 11:31:35 +0200

    Removed useless option
    Every option is now specified in the .json file.

--- Detectors/MUON/reconstruction/src/runMIDMaskGenerator.cxx
+++ Detectors/MUON/reconstruction/src/runMIDMaskGenerator.cxx
@@ -12,7 +12,6 @@ void addCustomOptions(bpo::options_description& options)
 {
   options.add_options()
   // ("help,h", "produces this usage message")
-  ("source,s", bpo::value<std::string>(), "address to get the messages from")
   ("binmapfile", bpo::value<std::string>(), "file with segmentation")
   ;
 }
