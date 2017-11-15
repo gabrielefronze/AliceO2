b550fff697eec505fee439d06a714d22c4c2781b
Unknown <sucre.91@hotmail.it>
2017-11-15 10:39:30 +0100

    Using unsigned short where needed

--- Detectors/MUON/reconstruction/include/MUONReconstruction/MIDFilter.h
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDFilter.h
@@ -35,8 +35,8 @@ namespace AliceO2 {
         private:
 
             struct stripMask {
-                UShort_t nDead; // number of elements for deadStripsIDs
-                UShort_t nNoisy; // number of elements for noisyStripsIDs
+                unsigned short nDead; // number of elements for deadStripsIDs
+                unsigned short nNoisy; // number of elements for noisyStripsIDs
                 std::unordered_set<uint32_t> deadStripsIDs; // container of UniqueIDs of dead strips
                 std::unordered_set<uint32_t> noisyStripsIDs; // container of UniqueIDs of noisy strips
             };
--- Detectors/MUON/reconstruction/include/MUONReconstruction/MIDRatesComputer.h
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDRatesComputer.h
@@ -37,8 +37,8 @@ namespace AliceO2 {
             OccupancyMapping fMapping;
 
             struct stripMask{
-                short nDead; // number of elements for deadStripsIDs
-                short nNoisy; // number of elements for noisyStripsIDs
+                unsigned short nDead; // number of elements for deadStripsIDs
+                unsigned short nNoisy; // number of elements for noisyStripsIDs
                 std::unordered_set<uint32_t> deadStripsIDs; // container of UniqueIDs of dead strips
                 std::unordered_set<uint32_t> noisyStripsIDs; // container of UniqueIDs of noisy strips
             };
