d30b67a9f2b6177d46053386613886828253d7f3
Unknown <sucre.91@hotmail.it>
2017-11-16 18:40:40 +0100

    Removing logic to exclude empty messages

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/Broadcaster.h
@@ -0,0 +1,42 @@
+#ifndef O2_DEV_ALO_BROADCASTER_H
+#define O2_DEV_ALO_BROADCASTER_H
+
+///
+///  @file   Broadcaster
+///  @author Gabriele G. Fronzé <gfronze at cern.ch>
+///  @date   24 July 2017
+///  @brief  Device to perform BCast of the
+///
+
+#include "FairMQDevice.h"
+#include "FairMQChannel.h"
+#include "MUONBase/Chrono.h"
+#include <string>
+#include <vector>
+
+namespace AliceO2 {
+
+    namespace MUON {
+
+        class Broadcaster : public FairMQDevice {
+        public:
+            Broadcaster( bool waiting = true );
+            ~Broadcaster(){
+                LOG(INFO) << "Average performance: " << fChronometer.PrintStatus();
+            };
+            virtual void InitTask();
+
+        private:
+            bool Broadcast(FairMQMessagePtr &, int);
+
+            std::vector<std::string> fOutputChannelNames;
+            bool fWaiting;
+
+            Chrono fChronometer;
+        };
+    }
+
+}
+
+
+#endif //O2_DEV_ALO_BROADCASTER_H
