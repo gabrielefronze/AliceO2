8783da1375a024d6438ebdf0fbe9f9be18dde620
Unknown <sucre.91@hotmail.it>
2017-11-17 17:31:48 +0100

    Using updated copyright notice

---
+++ Detectors/MUON/base/test/testChrono.cxx
@@ -0,0 +1,60 @@
+//
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @author  Gabriele Gaetano Fronzé
+
+#define BOOST_TEST_DYN_LINK
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include "MUONBase/Chrono.h"
+
+BOOST_AUTO_TEST_SUITE(Chrono)
+
+    BOOST_AUTO_TEST_CASE(Average){
+        Chrono chronometer;
+
+        int nCalls = 10;
+        int sum = 0;
+        for (int jCall = 0; jCall < nCalls; ++jCall) {
+            chronometer.AddCall((double)jCall);
+            sum += jCall;
+        }
+
+        double avg = (double)sum/(double)nCalls;
+
+        BOOST_TEST( avg == chronometer.AvgCallTime() );
+    }
+
+    BOOST_AUTO_TEST_CASE(PrintStatus){
+        Chrono chronometer;
+
+        int nCalls = 10;
+        int sum = 0;
+        for (int jCall = 0; jCall < nCalls; ++jCall) {
+            chronometer.AddCall((double)jCall);
+            sum += jCall;
+        }
+
+        double avg = (double)sum/(double)nCalls;
+
+        auto str = chronometer.PrintStatus();
+        int strNCalls = 0;
+        double strAvg = 0.;
+        scanf(str.c_str(),"NCalls=%d AvgTime=%lf",&strNCalls,&strAvg);
+
+        BOOST_TEST( avg == strAvg );
+        BOOST_TEST( strNCalls == nCalls );
+    }
+
+BOOST_AUTO_TEST_SUITE_END()
+
--- Detectors/MUON/reconstruction/include/MUONReconstruction/MIDRatesComputer.h
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDRatesComputer.h
@@ -1,7 +1,19 @@
 #ifndef O2_DEV_ALO_MIDRATESCOMPUTER_H
 #define O2_DEV_ALO_MIDRATESCOMPUTER_H
 
+//
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
 ///
+/// @author  Gabriele Gaetano Fronzé
 ///  @file   MIDRatesComputer
 ///  @author Gabriele G. Fronzé <gfronze at cern.ch>
 ///  @date   24 July 2017
