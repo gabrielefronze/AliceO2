226d8470dd4a0e1055076a69c9c15f0d8e51a873
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2017-11-30 18:43:15 +0100

    Added testDeserializer test

---
+++ Detectors/MUON/base/test/testDeserializer.cxx
@@ -0,0 +1,25 @@
+//
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @author  Gabriele Gaetano Fronzé
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+#include <boost/test/data/test_case.hpp>
+#include "MUONBase/Chrono.h"
+
+BOOST_AUTO_TEST_SUITE(testDeserializer)
+
+BOOST_AUTO_TEST_SUITE_END()
+
