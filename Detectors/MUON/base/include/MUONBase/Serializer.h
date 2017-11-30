dbec533c09816ab61c5b9b478ec1ad0f28cecb9e
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2017-11-30 16:38:06 +0100

    Addition of Serializer class
    This will be used to create custom messages for testing purposes.

---
+++ Detectors/MUON/base/include/MUONBase/Serializer.h
@@ -0,0 +1,28 @@
+//
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+///  @file   Deserializer
+///  @author Gabriele G. Fronzé <gfronze at cern.ch>
+///  @date   24 July 2017
+///  @brief  Little deserializer app to pack MID messages
+///
+
+#ifndef SERIALIZER_H
+#define SERIALIZER_H
+
+
+class Serializer {
+
+};
+
+
+#endif //SERIALIZER_H
