eb6a1f45ea37ec6b3e38d7d6f3a9ec5da6161a08
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2017-11-30 18:29:16 +0100

    Serializer class

--- Detectors/MUON/base/include/MUONBase/Serializer.h
+++ Detectors/MUON/base/include/MUONBase/Serializer.h
@@ -20,9 +20,51 @@
 #define SERIALIZER_H
 
 
-class Serializer {
+#include "FairMQMessage.h"
 
-};
+namespace AliceO2 {
+
+    namespace MUON {
+
+        class Serializer {
+
+        public:
+
+            Serializer();
+            ~Serializer(){};
+
+            // Human readable data struct for data storage
+            struct serializerDataStruct {
+                uint32_t fDetElemID = 0;
+                uint32_t fBoardID = 0;
+                uint32_t fChannel = 0;
+                uint32_t fCathode = 0;
+                serializerDataStruct(uint32_t detElemID, uint32_t boardID, uint32_t channel, uint32_t cathode){
+                    fDetElemID = detElemID;
+                    fBoardID = boardID;
+                    fChannel = channel;
+                    fCathode = cathode;
+                }
+            };
+
+            inline void AddDigit(uint32_t detElemID, uint32_t boardID, uint32_t channel, uint32_t cathode){ fData.emplace_back(detElemID,boardID,channel,cathode); };
+            inline void AddDigit(serializerDataStruct dataStruct){ fData.emplace_back(dataStruct); };
+            FairMQMessage* GetMessage();
+
+
+        private:
+            // Const values to exclude first 100 bytes of message and perform deserialization
+            const uint32_t kHeaderLength = 100;
+            uint32_t fHeader[100];
+
+            // Internal data container
+            std::vector<serializerDataStruct> fData;
+
+        };
+
+    }
+
+}
 
 
 #endif //SERIALIZER_H
--- Detectors/MUON/base/src/Serializer.cxx
+++ Detectors/MUON/base/src/Serializer.cxx
@@ -13,3 +13,43 @@
 /// @author  Gabriele Gaetano Fronzé
 
 #include "MUONBase/Serializer.h"
+#include "FairMQDevice.h"
+#include "FairMQTransportFactory.h"
+
+using namespace AliceO2::MUON;
+
+//_________________________________________________________________________________________________
+Serializer::Serializer() {
+    for (int iHeader = 0; iHeader < kHeaderLength; ++iHeader) {
+        fHeader[iHeader] = (iHeader%2==0)?0xDEAD:0xBEEF;
+    }
+}
+
+//_________________________________________________________________________________________________
+FairMQMessage* Serializer::GetMessage() {
+    // This vector will contain the full message
+    std::vector<uint32_t> OutputData;
+
+    // The first 100bytes are the header. It should remain the same.
+    OutputData.assign(fHeader, fHeader + 25);
+
+    // Putting in OutputData the translated structs
+    uint32_t digitBuffer = 0;
+    for( const auto &itData : fData ){
+        digitBuffer = 0;
+
+        digitBuffer |= itData.fDetElemID;
+        digitBuffer |= itData.fBoardID << 12; //shift of 12 bits
+        digitBuffer |= itData.fChannel << 24; //shift of 24 bits
+        digitBuffer |= itData.fCathode << 30; //shift of 30 bits
+
+        OutputData.emplace_back(digitBuffer);
+    }
+
+    // Creating a message to send out
+    FairMQMessage* msgOut;
+    msgOut->Rebuild(OutputData.size());
+    msgOut->SetMessage(&OutputData[0],OutputData.size());
+
+    return msgOut;
+}
