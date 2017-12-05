6a10c71e594ff0052b437e464ef08c26e8c207f3
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2017-12-05 17:46:19 +0100

    Adding MIDRatesComputerAlgorithm class

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDRatesComputerAlgorithm.h
@@ -0,0 +1,22 @@
+//
+// Created by Gabriele Gaetano Fronzé on 05/12/2017.
+//
+
+#ifndef MIDRATESCOMPUTERALGORITHM_H
+#define MIDRATESCOMPUTERALGORITHM_H
+namespace o2
+{
+namespace muon
+{
+namespace mid
+{
+class MIDRatesComputerAlgorithm
+{
+  public:
+
+    bool ProcessData(void* payload, int);
+};
+} // namespace mid
+} // namespace muon
+} // namespace o2
+#endif // MIDRATESCOMPUTERALGORITHM_H
