5540d50b69a01f5dcf201c0f6f6958d6463176fa
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2017-12-06 22:02:16 +0100

    Creating MIDMaskGeneratorAlgorithm

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDMaskGeneratorAlgorithm.cxx
@@ -0,0 +1,5 @@
+//
+// Created by Gabriele Gaetano Fronzé on 06/12/2017.
+//
+
+#include "MIDMaskGeneratorAlgorithm.h"
---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDMaskGeneratorAlgorithm.h
@@ -0,0 +1,13 @@
+//
+// Created by Gabriele Gaetano Fronzé on 06/12/2017.
+//
+
+#ifndef ALICEO2_MIDMASKGENERATORALGORITHM_H
+#define ALICEO2_MIDMASKGENERATORALGORITHM_H
+
+class MIDMaskGeneratorAlgorithm
+{
+
+};
+
+#endif //ALICEO2_MIDMASKGENERATORALGORITHM_H
