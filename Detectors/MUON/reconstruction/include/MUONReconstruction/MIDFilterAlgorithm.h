79ada75fb9af28e20b431fbd858efd8f8143f1b9
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-01-30 17:30:25 +0100

    First commit for MIDFilterAlgorithm

---
+++ Detectors/MUON/reconstruction/include/MUONReconstruction/MIDFilterAlgorithm.h
@@ -0,0 +1,28 @@
+//
+// Copyright CERN and copyright holders of ALICE O2. This software is
+// distributed under the terms of the GNU General Public License v3 (GPL
+// Version 3), copied verbatim in the file "COPYING".
+//
+// See https://alice-o2.web.cern.ch/ for full licensing information.
+//
+// In applying this license CERN does not waive the privileges and immunities
+// granted to it by virtue of its status as an Intergovernmental Organization
+// or submit itself to any jurisdiction.
+
+///
+/// @author  Gabriele Gaetano Fronzé
+///  @file   MIDFilterAlgorithm
+///  @author Gabriele G. Fronzé <gfronze at cern.ch>
+///  @date   30 Jan 2018
+///  @brief  Algorithm to compute rates based on occupancy information for MID
+///
+
+#ifndef ALICEO2_MIDFILTERALGORITHM_H
+#define ALICEO2_MIDFILTERALGORITHM_H
+
+class MIDFilterAlgorithm
+{
+
+};
+
+#endif //ALICEO2_MIDFILTERALGORITHM_H
