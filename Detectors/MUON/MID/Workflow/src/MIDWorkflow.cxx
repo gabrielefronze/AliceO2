aff065fdd8bce9a836edaec37f70ae6ef8f13f9a
Gabriele Gaetano Fronzé <gabriele.gaetano.fronze@cern.ch>
2018-07-21 16:18:24 +0200

    Update of MID workflow to include filtering devices

--- Detectors/MUON/MID/Workflow/src/MIDWorkflow.cxx
+++ Detectors/MUON/MID/Workflow/src/MIDWorkflow.cxx
@@ -13,9 +13,15 @@
 /// \author Gabriele G. Fronzé <gfronze at cern.ch>
 /// \date   11 July 2018
 
+#include <DataFormatsMID/ColumnData.h>
 #include "Framework/WorkflowSpec.h"
 #include "Framework/ConfigContext.h"
 #include "Framework/runDataProcessing.h"
+#include "Framework/DPLBoostSerializer.h"
+#include "Utils/Utils.h"
+#include "HitCounterSpec.h"
+#include "MaskGeneratorSpec.h"
+#include "FilterSpec.h"
 #include "ClusterizerSpec.h"
 #include "ColdataStreamerSpec.h"
 #include "TrackerSpec.h"
@@ -28,6 +34,13 @@ of::WorkflowSpec defineDataProcessing(const of::ConfigContext& configContext)
   of::WorkflowSpec workflow;
 
   workflow.emplace_back(o2::mid::getColdataStreamerSpec());
+  workflow.emplace_back(o2::workflows::defineBroadcaster("ColdataBcast",
+                                                         of::InputSpec{ "mid_coldata", "MID", "COLDATA" },
+                                                         of::Outputs{ of::OutputSpec{ "MID", "COLDATA_to_F" },
+                                                                      of::OutputSpec{ "MID", "COLDATA_to_HC" } }));
+  workflow.emplace_back(o2::mid::getHitCounterSpec());
+  workflow.emplace_back(o2::mid::getMaskGeneratorSpec());
+  workflow.emplace_back(o2::mid::getFilterSpec());
   workflow.emplace_back(o2::mid::getClusterizerSpec());
   workflow.emplace_back(o2::mid::getTrackerSpec());
   workflow.emplace_back(o2::mid::getSinkSpec());
